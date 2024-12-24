f807ee149b337a38f3b72d0276cb7bcafe8cfcac
mountain-kangkang <rngl10@naver.com>
2024-12-24 15:17:16 +0900

    last test plz

--- src/main.py
+++ src/main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 def root_handler():
     return {"message": "Hello World"}
+
+
+# GitHub Actions에서 실행을 위한 추가 코드
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
