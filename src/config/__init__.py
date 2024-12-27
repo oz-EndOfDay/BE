d5d3c41c288573bec54eb3a0cc2415cc4372070a
mountain-kangkang <rngl10@naver.com>
2024-12-27 13:43:07 +0900

    plz final

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,11 +1,12 @@
 import os
 from enum import StrEnum
-from typing import Any, Dict
+from typing import Any, Dict, TypeVar
 
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 load_dotenv()
+T = TypeVar("T", bound="Settings")
 
 
 class ServerEnv(StrEnum):
@@ -22,6 +23,6 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
     @classmethod
-    def get_config(cls) -> Dict[str, Any]:
+    def get_config(cls: type[T]) -> Dict[str, Any]:
         settings = cls()
         return settings.model_dump()
