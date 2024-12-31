a965d423c5e183ad252c3be83f93c14906236657
DevMon724 <devmon724@gmail.com>
2024-12-31 15:56:46 +0900

    build : api, schema 패키지에 파일 추가
    api
    ㄴrouter.py (추가)
    
    schema
    ㄴrequest.py(추가)
    ㄴresponse.py(추가)

---
+++ src/friend/models.py
@@ -0,0 +1,23 @@
+import re
+from datetime import datetime
+from typing import Type, TypeVar
+
+from sqlalchemy import Column, DateTime, Integer, Boolean, ForeignKey
+
+from src.config.database.orm import Base
+
+T = TypeVar("T", bound="Friend")  # Generic type variable for the class method
+
+class Friend(Base):
+    __tablename__ = "friends"
+
+    id = Column(Integer, primary_key=True, index=True)
+    is_accept = Column(Boolean, default=False)
+    user_id1 = Column(Integer, ForeignKey("users.id"), nullable=False)
+    user_id2 = Column(Integer, ForeignKey("users.id"), nullable=False)
+    ex_diary_cnt = Column(Integer, default=0)
+    last_ex_date = Column(DateTime, default=datetime.now)
+
+
+
+__all__ = ["Friend", "Base"]
