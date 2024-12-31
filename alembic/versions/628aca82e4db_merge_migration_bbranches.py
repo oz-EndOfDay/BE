44f22821b7e0d3eebb2b011776b0aefbbf334fe5
mountain-kangkang <rngl10@naver.com>
2024-12-31 22:33:44 +0900

    b, i, m,,, test

--- alembic/versions/628aca82e4db_merge_migration_bbranches.py
+++ alembic/versions/628aca82e4db_merge_migration_bbranches.py
@@ -5,15 +5,16 @@ Revises: 326f43b178a6, e14c81c06f61
 Create Date: 2024-12-31 22:27:00.879281
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '628aca82e4db'
-down_revision: Union[str, None] = ('326f43b178a6', 'e14c81c06f61')
+revision: str = "628aca82e4db"
+down_revision: Union[str, None] = ("326f43b178a6", "e14c81c06f61")
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
